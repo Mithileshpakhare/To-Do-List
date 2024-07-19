676b9f8ea7718f01882e71bb6fe3fc44a656483b
Mithilesh Pakhare <131905189+Mithileshpakhare@users.noreply.github.com>
2024-07-19 19:22:46 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,1 @@
-import { initializeApp} from "firebase/app";
-import { getFirestore} from "firebase/firestore";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCzf995JeOeZkLZIHg8TST_Sa_OzK3VZBo",
-    authDomain: "todo-crud-78dd1.firebaseapp.com",
-    projectId: "todo-crud-78dd1",
-    storageBucket: "todo-crud-78dd1.appspot.com",
-    messagingSenderId: "585166534517",
-    appId: "1:585166534517:web:1fcfb1873a0728843c053c"
-  };
-
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
-
-  export { db };
+Add firebase configuration for the database.
